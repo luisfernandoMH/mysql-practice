0824bee9a9ca673b566efc9d72749dd631165f70
luisfernandoMH <luisfernandomelchorhernandez@gmail.com>
2021-11-23 23:27:35 -0600

    ejercicio 1

--- exersice-1/exersice-1.sql
+++ exersice-1/exersice-1.sql
@@ -1,1 +1,22 @@
 --Here goes your code
+CREATE DATABASE store_db;
+
+CREATE TABLE people (
+id_person INT,
+first_name  char(20),
+last_name char(20),
+age INT
+);
+
+INSERT INTO people(first_name, last_name, age) VALUES  ( "Ana", "Olvera", 20);
+
+
+INSERT INTO people(first_name, last_name, age) VALUES  ( "Juan", "Alcantara", 17),
+
+INSERT INTO people(first_name, last_name, age) VALUES  ( "Laura", "Casareal", 15),
+("Jose",	"Torres", 16),
+("Maria", "Arias",17);
+
+SELECT * FROM store_db.people;
+
+DROP TABLE people;
